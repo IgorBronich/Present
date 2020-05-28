1b6580d41ae07a7e79ea446bca293c511271102a
Igor <igorek08@inbox.ru>
2020-05-28 15:38:49 +0700

    Almost ready program

--- src/com/epam/candies/view/ConsolePresentPrinter.java
+++ src/com/epam/candies/view/ConsolePresentPrinter.java
@@ -2,19 +2,30 @@ package com.epam.candies.view;
 
 import com.epam.candies.PresentFactory;
 import com.epam.candies.logic.PresentCalculator;
+import com.epam.candies.model.Candy;
 import com.epam.candies.model.Present;
 import org.w3c.dom.ls.LSOutput;
+
+import java.util.List;
 
 public class ConsolePresentPrinter implements PresentPrinter {
 
     @Override
     public void print(Present present) {
-        PresentCalculator presentCalculator = new PresentCalculator();
 
+
+        List<Candy> list = present.getCandyList();
+
+        for(Candy candy: list){
+            System.out.println(candy.getName() + " " + candy.getWeight() + " грамм, " + candy.getPrice() + " рублей.");
+        }
+
+        System.out.println(" ");
+
+        PresentCalculator presentCalculator = new PresentCalculator();
         double weight = presentCalculator.calculateWeight(present);
         double price = presentCalculator.calculatePrice(present);
-
-        System.out.println("Вес подарка = " + weight + " грамм.");
+        System.out.println("Вес всего подарка = " + weight + " грамм.");
         System.out.println("Стоимость подарка = " + price + " рублей.");
     }
 }
